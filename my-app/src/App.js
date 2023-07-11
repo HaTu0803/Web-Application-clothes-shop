c38adb62693d45970bdc3469c03784600826c08e
nhinguyen1011 <75206214+nhinguyen1011@users.noreply.github.com>
2023-07-11 16:43:48 +0700

    Add files via upload

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import './index.css';
 import Header from './components/Header';
-import Slide from './components/Slide';
-import Service from './components/Service';
-import CardList from './components/CardList';
-import CardDetail from './components/CardDetail';
-import SideContent from './components/SideContent';
-import Sales from './components/Sales';
+// import Slide from './components/Slide';
+// import Service from './components/Service';
+// import CardList from './components/CardList';
+// import CardDetail from './components/CardDetail';
+// import SideContent from './components/SideContent';
+// import Sales from './components/Sales';
 import Footer from './components/Footer';
 
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import {ToastContainer } from "react-toastify";
+
+import Home from "./components/carts/Home";
+import NotFound from "./components/carts/NotFound";
+import Cart from "./components/carts/Cart";
+
+import "react-toastify/dist/ReactToastify.css";
 
 
 
@@ -16,16 +25,19 @@ function App() {
   return (
 
   <div className = "wrapper">
-    <Header />
-    <Slide />
-    <Service />
-    <CardList></CardList>
-    <CardDetail />
-    <SideContent />
-    <Sales />
-    <Footer />
 
-  
+        <ToastContainer />
+        <Header />
+        <div className="content-container">
+          <Routes>
+            <Route path="/cart" component={Cart} />
+            <Route path="/not-found" component={NotFound} />
+            <Route path="/" exact component={Home} />
+          </Routes>
+        </div>
+        <Footer />
+
+
   </div>
 
   )
