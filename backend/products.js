b80d08383b72fb079056f22a86b3a20134613480
nhinguyen1011 <75206214+nhinguyen1011@users.noreply.github.com>
2023-07-11 16:33:26 +0700

    Add files via upload

---
+++ backend/products.js
@@ -0,0 +1,31 @@
+const products = [
+  {
+    id: 1,
+    name: "Blouse",
+    brand: "H&M",
+    desc: "Pretty",
+    price: 999,
+    image:
+      "https://res.cloudinary.com/dxe0ybe4b/image/upload/v1689006895/OnlineShop/aoA_oxi5a2.jpg",
+  },
+  {
+    id: 2,
+    name: "Blouse",
+    brand: "Dior",
+    desc: "Cotton",
+    price: 699,
+    image:
+      "https://res.cloudinary.com/dxe0ybe4b/image/upload/v1689006894/OnlineShop/aoB_tifgle.jpg",
+  },
+  {
+    id: 3,
+    name: "Blouse",
+    brand: "Chanel",
+    desc: "Big",
+    price: 399,
+    image:
+      "https://res.cloudinary.com/dxe0ybe4b/image/upload/v1689006902/OnlineShop/aoC_uzje1o.jpg",
+  },
+];
+
+module.exports = products;
