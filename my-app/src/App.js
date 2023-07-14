2c77ecec34433c1d63ead32c9072453477d7dae8
nhinguyen1011 <75206214+nhinguyen1011@users.noreply.github.com>
2023-07-14 22:30:16 +0700

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,10 @@
 import React from 'react';
 import './index.css';
-import Header from './components/Header';
-// import Slide from './components/Slide';
-// import Service from './components/Service';
-// import CardList from './components/CardList';
-// import CardDetail from './components/CardDetail';
-// import SideContent from './components/SideContent';
-// import Sales from './components/Sales';
-import Footer from './components/Footer';
 
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import {ToastContainer } from "react-toastify";
+import Cart from './components/Cart';
+import Payment from './components/Payment';
 
-import Home from "./components/carts/Home";
-import NotFound from "./components/carts/NotFound";
-import Cart from "./components/carts/Cart";
-
-import "react-toastify/dist/ReactToastify.css";
+import {BrowserRouter ,Routes,Route} from "react-router-dom";
 
 
 
@@ -25,21 +12,20 @@ function App() {
   return (
 
   <div className = "wrapper">
+    
+    <BrowserRouter>
+      <Routes>
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/payment" element={<Payment />} />
 
-        <ToastContainer />
-        <Header />
-        <div className="content-container">
-          <Routes>
-            <Route path="/cart" component={Cart} />
-            <Route path="/not-found" component={NotFound} />
-            <Route path="/" exact component={Home} />
-          </Routes>
-        </div>
-        <Footer />
-
-
+      </Routes>
+    </BrowserRouter>
+  
   </div>
 
-  )
+    
+    
+  );
 };
+
 export default App;
