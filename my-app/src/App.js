04ded01887c61d7f7d7193680ac62232038e1ca1
dadikas <73420676+dadikas@users.noreply.github.com>
2023-07-15 09:03:49 +0700

    dropdown

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -24,6 +24,7 @@ function App() {
     <SideContent />
     <Sales />
     <Footer />
+
   </div>
 
   )
--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -12,19 +12,79 @@ const Body = () => {
                <div className="header">
         <a href = "index.html"><img src= {Logo} className="logo"/></a>
         <nav>
-        <ul className = "main-menu">
+        
+            <ul className = "main-menu">
+           
             <li><a href ="" className = "menu-text">Hàng Mới</a></li>
-            <li><a href =""  className = "menu-text">Sản Phẩm</a></li>
+            
+            <li className="dropdown"><a href =""  className = "menu-text">Sản Phẩm</a>
+                <div className="dropdown-content">
+                    <div className="Type1"><h5>GIÀY</h5>
+                        <ul className='Product'>
+                            <li className='Type'><a>Giày</a></li>
+                            <li className='Type'><a>Giày</a></li>
+                            <li className='Type'><a>Giày</a></li>
+                            <li className='Type'><a>Giày</a></li>
+                        </ul>
+                    </div>
+                    <div className="Type2"><h5>TÚI</h5>
+                        <ul className='Product'>
+                            <li className='Type'><a>Giày</a></li>
+                            <li className='Type'><a>Giày</a></li>
+                            <li className='Type'><a>Giày</a></li>
+                            <li className='Type'><a>Giày</a></li>
+                        </ul>
+                    </div>
+                    <div className="Type3"><h5>PHỤ KIỆN</h5>
+                        <ul className='Product'>
+                            <li className='Type'><a>Giày</a></li>
+                            <li className='Type'><a>Giày</a></li>
+                            <li className='Type'><a>Giày</a></li>
+                            <li className='Type'><a>Giày</a></li>
+                        </ul></div>
+                    <div className="Type4"><h5>QUẦN ÁO</h5>
+                    <ul className='Product'>
+                            <li className='Type'><a>Giày</a></li>
+                            <li className='Type'><a>Giày</a></li>
+                            <li className='Type'><a>Giày</a></li>
+                            <li className='Type'><a>Giày</a></li>
+                            <li className='Type'><a>Giày</a></li>
+                            <li className='Type'><a>Giày</a></li>
+                            <li className='Type'><a>Giày</a></li>
+                            <li className='Type'><a>Giày</a></li>
+                        </ul></div>
+                    <div className="Type5"><h5>BỘ SƯU TẬP</h5>
+                        <ul className='Product'>
+                            <li className='Type'><a>Giày</a></li>
+                            <li className='Type'><a>Giày</a></li>
+                            <li className='Type'><a>Giày</a></li>
+                            <li className='Type'><a>Giày</a></li>
+                            <li className='Type'><a>Giày</a></li>
+                            <li className='Type'><a>Giày</a></li>
+                            <li className='Type'><a>Giày</a></li>
+                            <li className='Type'><a>Giày</a></li>
+                            <li className='Type'><a>Giày</a></li>
+                            <li className='Type'><a>Giày</a></li>
+                            <li className='Type'><a>Giày</a></li>
+                            <li className='Type'><a>Giày</a></li>
+                        </ul></div>
+                </div>  
+            </li>            
+                              
             <li><a href =""  className = "menu-text">Bộ Sưu Tập</a></li>
             <li><a href =""  className = "menu-text">Liên Hệ</a></li>
+        
             <nav className = "menu-right">
+
             <input type="text" className = "search-input  mainLoginInput"  name="search" placeholder="Tìm Kiếm..."  />
             <a href ="" target ="_blank" className = "menu-text menu-icon"><FontAwesomeIcon icon={faSearch} /></a>
             <a href ="" target ="_blank" className = "menu-text menu-icon"><FontAwesomeIcon icon={faUser} /></a>
             <a href ="" target ="_blank" className = "menu-text menu-icon"><FontAwesomeIcon icon={faShoppingCart}/></a>
             </nav>
-        </ul>
-    </nav>
+            
+            </ul>
+        
+        </nav>
        </div>
         </div>
     );
