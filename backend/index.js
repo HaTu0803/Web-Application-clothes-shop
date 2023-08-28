0b85f49d49886fa5ab45a9bb7ff1dd822255f03c
nhinguyen1011 <75206214+nhinguyen1011@users.noreply.github.com>
2023-08-29 01:29:29 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,20 +6,23 @@
 import express from 'express';
 import productRoutes from './routes/product-route.js';
 import paymentRoutes from './routes/payment-routes.js';
+import bodyParser from 'body-parser';
 
 var app = express();
 app.set('view engine', 'ejs');
 
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
-app.use('/api', productRoutes); 
+app.use('/api', productRoutes);
 
-app.use('/payment', paymentRoutes); 
+app.use('/payment', paymentRoutes);
 
 const PORT = 5000;
 app.listen(PORT, function () {
