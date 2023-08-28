5fbf5bf7c5caa376e5e46f0687a158776bb258d7
nhinguyen1011 <75206214+nhinguyen1011@users.noreply.github.com>
2023-08-29 01:28:36 +0700

    Update payment-routes.js

--- backend/routes/payment-routes.js
+++ backend/routes/payment-routes.js
@@ -1,46 +1,26 @@
 import express from 'express';
-// const express = require('express');
 const router = express.Router();
 
 import paymentControllers from '../services/payment-service.js';
 
 
-router.post('/payment', async (req, res) => {
-    const pwd = req.body.pwd;
-    const email = req.body.email;
-    console.log("abc")
-
+router.post('/payment', async (req, res) => { 
     try {
-        const products = await paymentControllers.checkPassword(pwd,email);
-        return res.json(products.recordsets[0]);
-        console.log(products.recordsets[0])
+        const products = await paymentControllers.checkPassword(req.body.pwd, req.body.bankAccountNumber);
+        const accountDetails = await paymentControllers.PaytoCash(req.body.user,req.body.money);
+
+        if(accountDetails.accountBalance >= req.body.money){
+            console.log("Thành công")
+            req.body.money = accountDetails.accountBalance - req.body.money
+        }
+        else{
+            console.log("Thất bại")
+        }
+
     } catch (error) {
         console.log(error)
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
-//     router.get('/payment', async (req, res) => {
-//         try {
-//             const products = await paymentControllers.PayToCash(type);
-//             return res.json(products.recordsets[0]);
-//         } catch (error) {
-//             console.log(error)
-//             res.status(500).json({ error: 'Internal Server Error' });
-//         }
-// });
-// router.get('/checkpassword', async (req, res) => {
-//     paymentModel.checkPasswordModel(req.body.UserID, (err,data)=>{
-//         if(err) res.json({err: true})
-//         else{
-//             if(bcrypt.compareSync(req.body.BankAccountNumber, data.BankAccountNumber))
-//             {
-//                 res.json({err: false})
-//                 return
-//             }
-//             res.json({err: true})
-//         }
-//     })
-
-// });
 
 export default router;
