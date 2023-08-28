d7b264bff5bd93975eeaf62ba67ba01806411e03
nhinguyen1011 <75206214+nhinguyen1011@users.noreply.github.com>
2023-08-28 19:31:41 +0700

    Update Payment.js

--- my-app/src/components/Payment/Payment.js
+++ my-app/src/components/Payment/Payment.js
@@ -1,123 +1,127 @@
-import React from 'react';
-import './Payment.css';
-import { FaCcVisa, FaCcMastercard, FaCcDiscover } from 'react-icons/fa';
-
+import React from "react";
+import "./Payment.css";
+import { FaCcVisa, FaCcMastercard, FaCcDiscover } from "react-icons/fa";
+import { useState, useEffect } from "react";
 
 const Payment = () => {
-    return (
-        <>
-            <div className="container">
+    const [formData, setFormData] = useState({
+        email: '',
+        pwd: ''
+    });
+  const [productBuy, setproductBuy] = useState([]);
+  const [total, setTotal] = useState(0);
+  useEffect(() => {
+    const cart = localStorage.getItem("cart");
+    if (cart) {
+      setproductBuy(JSON.parse(cart));
+      const total = JSON.parse(cart)?.reduce((total, product) => {
+        return total + product.price * product.quantity;
+      }, 0);
+      setTotal(total);
+    }
+  }, []);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-                <div className="row">
-                    <div className="col-75">
-                        <div className="container">
-                            <form action="/action_page.php">
+    
+    try {
+        const response = await fetch('http://localhost:3001/api/payment', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+        });
 
-                                <div className="row">
-                                    <div className="col-50">
-                                        <h3>Billing Address</h3>
-                                        <label for="fname"> Nhập họ tên</label>
-                                        <input type="text" id="fname" name="firstname" placeholder="Họ và tên" />
-                                        <label for="email">Nhập email</label>
-                                        <input type="text" id="email" name="email" placeholder="Email" />
-                                        <label for="adr"> Nhập số điện thoại</label>
-                                        <input type="tel" id="num" name="number" />
+        if (response.ok) {
+            console.log('Data sent successfully');
+        } else {
+            console.error('Error sending data');
+        }
+    } catch (error) {
+        console.error(error);
+    }
+    // console.log(e);
+};
+  return (
+    <>
+      <div class="container">
+        <form method="POST">
+          <div class="payment">
+            <div class="row">
+              <div class="col-50">
+                <h3>Billing Address</h3>
+                <label for="fname"> Nhập tên</label>
+                <input
+                  type="text"
+                  id="fname"
+                  name="firstname"
+                  placeholder="Họ và tên"
+                />
+                <label for="email">Nhập mã thẻ</label>
+                <input
+                  type="text"
+                  id="fname"
+                  name="email"
+                  placeholder="Nhập mã thẻ"
+                />
+                <label for="adr"> Nhập mật khẩu </label>
+                <input type="password" id="pwd" name="pwd" />
+              </div>
 
-
-                                        <div className="row">
-                                            <div className="col-25">
-                                                <label for="city">Thành phố</label>
-                                                <div className="custom-city" style={{ width: '200px' }}>
-                                                    <select>
-                                                        <option value="0">Chọn tỉnh/thành</option>
-                                                        <option value="1">TP Hồ Chí Minh</option>
-                                                        <option value="2">Đà Nẵng</option>
-
-                                                    </select>
-                                                </div>
-
-                                            </div>
-                                            <div className="col-25">
-                                                <label for="city">Quận/huyên</label>
-
-                                                <div className="custom-dis" style={{ width: '200px' }}>
-                                                    <select>
-                                                        <option value="0">Chọn quận/huyên</option>
-                                                        <option value="1">Hóc Môn</option>
-                                                        <option value="2">Quận 5</option>
-
-                                                    </select>
-                                                </div>
-
-                                            </div>
-                                            <div className="col-25">
-                                                <label for="city"> Phường/xã</label>
-
-                                                <div className="custom-com" style={{ width: '200px' }}>
-                                                    <select>
-                                                        <option value="0">Chọn phường/xã</option>
-                                                        <option value="1">Xuân Thới Thượng</option>
-                                                        <option value="2">4</option>
-
-                                                    </select>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div className="col-50">
-                                        <h3>Payment</h3>
-                                        <label for="fname">Các loại thẻ</label>
-                                        <div className="icon-container">
+              <div class="col-50">
+                {/* <label for="fname">Các loại thẻ</label>
+                                        <div class="icon-container">
                                             <form>
                                                 <input name="gender" type="radio" /> <FaCcVisa style={{ color: 'navy' }} />
                                                 <input name="gender" type="radio" /><FaCcMastercard style={{ color: 'red' }} />
-                                                <input name="gender" type="radio" /> <FaCcDiscover style={{ color: 'orange' }} />
                                             </form>
 
-                                        </div>
-                                        <label for="cname">Tên chủ thẻ</label>
-                                        <input type="text" id="cname" name="cardname" placeholder="Tên" />
-                                        <label for="ccnum">Số thẻ</label>
-                                        <input type="text" id="ccnum" name="cardnumber" placeholder="1111-2222-3333-4444" />
-                                        <label for="expmonth">Exp Month</label>
-                                        <input type="number" id="expmonth" name="expmonth" />
-                                        <div className="row">
-                                            <div className="col-50">
-                                                <label for="expyear">Exp Year</label>
-                                                <input type="text" id="expyear" name="expyear" placeholder="2018" />
-                                            </div>
-                                            <div className="col-50">
-                                                <label for="cvv">CVV</label>
-                                                <input type="text" id="cvv" name="cvv" placeholder="352" />
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                </div>
-                                <label>
-                                    <input type="checkbox" name="sameadr" /> Shipping address same as billing
-                                </label>
-                                <input type="submit" value="Tiếp tục thanh toán" className="btn" />
-                            </form>
-                        </div>
-                    </div>
-                    <div className="col-25">
-                        <div className="container">
-                            <h4>Cart <span className="price" style={{ color: 'black' }}><i className="fa fa-shopping-cart"></i> <b>4</b></span></h4>
-                            <p><a href="#">Balo Satchel Trang Trí Khóa</a> <span className="price">$15</span></p>
-                            <p><a href="#">Sơ mi trắng</a> <span className="price">$5</span></p>
-                            <p><a href="#">Áo thun</a> <span className="price">$8</span></p>
-                            <p><a href="#">Quần jean</a> <span className="price">$2</span></p>
-                            <hr />
-                            <p>Total <span className="price" style={{ color: 'black' }}><b>$30</b></span></p>
-                        </div>
-                    </div>
-                </div>
-
+                                        </div> */}
+                <label>
+                  <input type="checkbox" checked="checked" name="sameadr" />{" "}
+                  Shipping address same as billing
+                </label>
+              </div>
             </div>
-        </>
-    );
-}
+          </div>
+          {/* <input type="submit" value="Thanh toán" class="btnt" /> */}
+          <div class="payment">
+            <div class="row">
+              <div class="col-50">
+                <h4>
+                  Cart
+                  <span class="price" style={{ color: "black" }}>
+                    <i class="fa fa-shopping-cart"></i>
+                  </span>
+                </h4>
+
+                {productBuy.map((product, index) => (
+                  <div key={index}>
+                    <p>
+                      {product.name} <span class="price">{product.price}</span>
+                    </p>
+                  </div>
+                ))}
+                <p>
+                  Total
+                  <span class="price" style={{ color: "black" }}>
+                    <b>{total}</b>
+                  </span>
+                </p>
+                <input
+                  type="submit"
+                  value="Thanh toán"
+                  class="btn"
+                  onClick={handleSubmit}
+                />
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
 
 export default Payment;
