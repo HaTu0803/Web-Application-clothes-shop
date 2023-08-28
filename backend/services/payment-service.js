455858ffcb6740d1f663cd29e525e83182d937b5
nhinguyen1011 <75206214+nhinguyen1011@users.noreply.github.com>
2023-08-29 01:29:01 +0700

    Update payment-service.js

--- backend/services/payment-service.js
+++ backend/services/payment-service.js
@@ -1,21 +1,10 @@
 import sql from 'mssql'
+import config from '../dbconfig.js'
 
-var config = {
-  server: "localhost",
-  port: 1433, // Make sure to escape backslashes in the server name
-  database: "shopping_website", // Corrected "databse" to "database"
-  user: 'sa',
-  password: "123456aA@$",
-  options: {
-    enableArithAbort: true,
-    trustServerCertificate: true,
-  },
-  connectionTimeout: 150000,
-  pool:
-    { max: 10, min: 0, idleTimeoutMillis: 30000, },
-};
+var pool = await sql.connect(config)
 
 export default {
+
   // async PayToCash(AccountBalance) {
   //   // let pool = await sql.connect(config)
   //   // let result1 = await pool.request().query(`select * from bankaccounts where AccountBalance = ${AccountBalance}`)
@@ -24,11 +13,13 @@ export default {
 
   // },
 
-  async checkPassword(pwd,email){
-    // let pool = await sql.connect(config)
-    // let result1 = await pool.request().query(`select u.pass from USERS u, bankaccounts b where u.pass = ${pass} and BankAccountNumber = ${BankAccountNumber}`  )
-    console.log("ok1")
-    // return result1
-    }
-   
+  async checkPassword(pwd, bankAccountNumber) {
+    let result = await pool.request().query(`SELECT DISTINCT u.pass, b.BankAccountNumber FROM USERS u, bankaccounts b WHERE u.pass = ${pwd} and b.BankAccountNumber =  ${bankAccountNumber}`)
+    return result;
+  }, 
+  async PaytoCash(user, money) {
+    let result = await pool.request().query(`SELECT DISTINCT u.pass, b.AccountBalance FROM bankaccounts b,USERS u  where u.UserName = ${user} and b.AccountBalance =  ${AccountBalance}`)
+    return result;
+  }
+
 }
